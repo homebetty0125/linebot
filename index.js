3f16d2df0b9ec4a8b46255ce04892d1e80f94600
bliang.tw <bliang.tw@moonshine.ad>
2021-05-12 14:47:24 +0800

    - Test

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const linebot = require('linebot');
 const express = require('express');
-const app = express();
 
 // Line Channel 資訊
 const bot = linebot({
@@ -9,14 +8,14 @@ const bot = linebot({
     channelAccessToken: 'IukFQyWUmQz4ZKE9ij48Q6bzhDYIQbfPzZ0ZBU6Tmr8XK+4eRC6CN6YatVmDJSJ+dT696SH7LwI7V+oDPQFvUUzR+3MoAHdq53WSsZZameVwM/TcSnxLIyhyBxDDgT50zvHswT83QV1xLWClRqXHcAdB04t89/1O/w1cDnyilFU='
 });
 
-bot.on('message', function (e) {
+bot.on('message', function (event) {
 
-    const replyMesg = `Hi~ 你剛才說的是:${e.message.text}`;
+    const replyMesg = `Hi~ 你剛才說的是:${event.message.text}`;
 
     console.log('replyMesg:', replyMesg)
 
-    // e.message.text是使用者傳給bot的訊息
-    e.reply(replyMesg)
+    // event.message.text是使用者傳給bot的訊息
+    event.reply(replyMesg)
         .then(function (data) {
 
             // 當訊息成功回傳後的處理
@@ -32,15 +31,27 @@ bot.on('message', function (e) {
 
 });
 
+const app = express();
 const linebotParser = bot.parser();
-app.post('/linewebhook', linebotParser);
+app.post('/', linebotParser);
 
-// Bot 所監聽的 webhook 路徑與 port
-app.listen('/linewebhook', 3000);
+const server = app.listen(process.env.port || 8080, function () {
+
+    const port = server.address().port;
+    console.log('now running on port:', port);
+
+});
+
+// bot.listen('/linewebhook', 3000);
 
 /**
  * Reference
- * Heroku: https://developers.line.biz/zh-hant/docs/messaging-api/building-sample-bot-with-heroku/#deploy-the-echo-sample-bot
- * ngrok: https://medium.com/pyradise/%E4%BD%BF%E7%94%A8node-js%E5%BB%BA%E7%BD%AE%E4%BD%A0%E7%9A%84%E7%AC%AC%E4%B8%80%E5%80%8Bline-bot-590b7ba7a28a
- * https://ithelp.ithome.com.tw/articles/10216236
+ * [Heroku]
+ *      https://www.oxxostudio.tw/articles/201701/line-bot.html
+ *      https://developers.line.biz/zh-hant/docs/messaging-api/building-sample-bot-with-heroku/#deploy-the-echo-sample-bot
+ * [Bottender]
+ *      https://blog.yoctol.com/%E4%BB%A5-bottender-v1-%E6%92%B0%E5%AF%AB%E7%B0%A1%E5%96%AE%E7%9A%84%E5%8D%A1%E7%B1%B3%E7%8B%97-938243e2dc46
+ * [ngrok]
+ *      https://medium.com/pyradise/%E4%BD%BF%E7%94%A8node-js%E5%BB%BA%E7%BD%AE%E4%BD%A0%E7%9A%84%E7%AC%AC%E4%B8%80%E5%80%8Bline-bot-590b7ba7a28a
+ *      https://ithelp.ithome.com.tw/articles/10216236
  */
