5a31d9d9826a587236c1bedc74878fd344bdcdda
bliang.tw <bliang.tw@moonshine.ad>
2021-05-12 19:12:34 +0800

    - Test

--- index.js
+++ index.js
@@ -1,55 +1,9 @@
 const linebot = require('linebot');
 const express = require('express');
-
 const webhookRouter = require('./routes/webhook');
-
 const app = express();
 
 app.use('/webhook', webhookRouter);
-
-// Line Channel 資訊
-// const bot = linebot({
-//     channelId: 1552161500,
-//     channelSecret: '5f4e117a76c9d6515203a4d837168885',
-//     channelAccessToken: 'IukFQyWUmQz4ZKE9ij48Q6bzhDYIQbfPzZ0ZBU6Tmr8XK+4eRC6CN6YatVmDJSJ+dT696SH7LwI7V+oDPQFvUUzR+3MoAHdq53WSsZZameVwM/TcSnxLIyhyBxDDgT50zvHswT83QV1xLWClRqXHcAdB04t89/1O/w1cDnyilFU='
-// });
-
-// bot.on('message', function (event) {
-
-//     const replyMesg = `Hi~ 你剛才說的是:${event.message.text}`;
-
-//     console.log('replyMesg:', replyMesg)
-
-//     // event.message.text是使用者傳給bot的訊息
-//     event.reply(replyMesg)
-//         .then(function (data) {
-
-//             // 當訊息成功回傳後的處理
-//             console.log('data:', data);
-
-//         })
-//         .catch(function (error) {
-
-//             // 當訊息回傳失敗後的處理
-//             console.log('error:', error);
-
-//         });
-
-// });
-
-// const app = express();
-// const linebotParser = bot.parser();
-// app.post('/', linebotParser);
-
-// const server = app.listen(process.env.port || 8080, function () {
-
-//     const port = server.address().port;
-//     console.log('now running on port:', port);
-
-// });
-
-// bot.listen('/linewebhook', 3000);
-
 
 
 /**
