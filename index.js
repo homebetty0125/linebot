684662824a64adad765cfffdcd8cf425530200f8
bliang.tw <bliang.tw@moonshine.ad>
2021-05-13 19:10:51 +0800

    - Update line bot test

--- index.js
+++ index.js
@@ -1,41 +1,27 @@
 const express = require('express');
-const webhookRouter = require('./routes/webhook');
 const app = express();
-
+const webhookRouter = require('./routes/webhook');
 const port = process.env.PORT || 3005;
 
 app.use('/webhook', webhookRouter);
 
-// app.get('/', (reqData, resData) => {
+// 監聽 port
+app.listen(port);
+// app.listen(port, () => {
 
-//     // 文字
-//     // resData.send(`<hi>Hiiiii</hi>`);
-
-//     // JSON
-//     resData.json({
-//         id: 1,
-//         name: 'Betty',
-//     });
+//     console.log(`Express is running on http://localhost:${port}`);
 
 // });
-
-// console.log('port:', process.env.PORT);
-
-app.listen(port, () => {
-
-    console.log(`Express is running on http://localhost:${port}`);
-
-});
 
 
 /**
  * Reference
- * [Heroku]
+ * [Heroku] 遠端
  *      https://www.oxxostudio.tw/articles/201701/line-bot.html
  *      https://developers.line.biz/zh-hant/docs/messaging-api/building-sample-bot-with-heroku/#deploy-the-echo-sample-bot
- * [Bottender]
- *      https://blog.yoctol.com/%E4%BB%A5-bottender-v1-%E6%92%B0%E5%AF%AB%E7%B0%A1%E5%96%AE%E7%9A%84%E5%8D%A1%E7%B1%B3%E7%8B%97-938243e2dc46
- * [ngrok]
+ * [ngrok] 本機
  *      https://medium.com/pyradise/%E4%BD%BF%E7%94%A8node-js%E5%BB%BA%E7%BD%AE%E4%BD%A0%E7%9A%84%E7%AC%AC%E4%B8%80%E5%80%8Bline-bot-590b7ba7a28a
  *      https://ithelp.ithome.com.tw/articles/10216236
+ * [Bottender]
+ *      https://blog.yoctol.com/%E4%BB%A5-bottender-v1-%E6%92%B0%E5%AF%AB%E7%B0%A1%E5%96%AE%E7%9A%84%E5%8D%A1%E7%B1%B3%E7%8B%97-938243e2dc46
  */
--- routes/webhook.js
+++ routes/webhook.js
@@ -2,34 +2,34 @@ const express = require('express');
 const router = express.Router();
 const linebot = require('linebot');
 
-console.log('webhook.js is working');
-
 const bot = linebot({
     channelId: 1552161500,
     channelSecret: '5f4e117a76c9d6515203a4d837168885',
     channelAccessToken: 'IukFQyWUmQz4ZKE9ij48Q6bzhDYIQbfPzZ0ZBU6Tmr8XK+4eRC6CN6YatVmDJSJ+dT696SH7LwI7V+oDPQFvUUzR+3MoAHdq53WSsZZameVwM/TcSnxLIyhyBxDDgT50zvHswT83QV1xLWClRqXHcAdB04t89/1O/w1cDnyilFU='
 });
 
-bot.on('message', function (event) {
+bot.on('message', (event) => {
 
-    const replyMesg = `Hi~ 你剛才說的是:${event.message.text}`;
+    console.log('event:', event);
 
-    console.log('replyMesg:', replyMesg)
+    const mesg = `Hi~ 你剛才說的是:${event.message.text}`;
+
+    // console.log('mesg:', mesg)
 
     // event.message.text是使用者傳給bot的訊息
-    event.reply(replyMesg)
-        .then(function (data) {
+    // event.reply(mesg)
+    //     .then((data) => {
 
-            // 當訊息成功回傳後的處理
-            console.log('data:', data);
+    //         // 當訊息成功回傳後的處理
+    //         console.log('data:', data);
 
-        })
-        .catch(function (error) {
+    //     })
+    //     .catch((error) => {
 
-            // 當訊息回傳失敗後的處理
-            console.log('error:', error);
+    //         // 當訊息回傳失敗後的處理
+    //         console.log('error:', error);
 
-        });
+    //     });
 
 });
 
@@ -37,3 +37,8 @@ const linebotParser = bot.parser();
 router.post('/', linebotParser);
 
 module.exports = router;
+
+/**
+ * [LINE Bot 回應]
+ *      https://ithelp.ithome.com.tw/articles/10229397
+ */
